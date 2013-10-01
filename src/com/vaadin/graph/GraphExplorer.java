a4e6f2fd0a7b1be8dc9198f53018852565fd4254
Marlon Richert <marlon@vaadin.com>
2013-10-01 16:24:12 +0300

    Apache License, Version 2.0

--- src/com/vaadin/graph/GraphExplorer.java
+++ src/com/vaadin/graph/GraphExplorer.java
@@ -1,17 +1,17 @@
 /*
- * Copyright 2011 Vaadin Ltd.
- *
- * Licensed under the GNU Affero General Public License, Version 3.0 (the
- * "License"); you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.gnu.org/licenses/agpl-3.0.html
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
+ * Copyright 2011-2013 Vaadin Ltd.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not 
+ * use this file except in compliance with the License. You may obtain a copy of 
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0.html
+ * 
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT 
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the 
+ * License for the specific language governing permissions and limitations under 
+ * the License. 
  */
 package com.vaadin.graph;
 
@@ -35,7 +35,7 @@ public class GraphExplorer<N extends Node, A extends Arc> extends
     int clientWidth = 0;
     transient final GraphController<N, A> controller;
 
-    private GraphModel model;
+    private final GraphModel model;
 
     public GraphExplorer(GraphRepository<N, A> repository) {
         this.controller = new GraphController<N, A>(repository);
--- src/com/vaadin/graph/GraphModel.java
+++ src/com/vaadin/graph/GraphModel.java
@@ -1,17 +1,17 @@
 /*
- * Copyright 2011 Vaadin Ltd.
- *
- * Licensed under the GNU Affero General Public License, Version 3.0 (the
- * "License"); you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.gnu.org/licenses/agpl-3.0.html
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
+ * Copyright 2011-2013 Vaadin Ltd.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not 
+ * use this file except in compliance with the License. You may obtain a copy of 
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0.html
+ * 
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT 
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the 
+ * License for the specific language governing permissions and limitations under 
+ * the License. 
  */
 package com.vaadin.graph;
 
@@ -31,7 +31,7 @@ class GraphModel {
     private final Map<String, NodeProxy> nodes = new HashMap<String, NodeProxy>();
     private final Map<String, ArcProxy> arcs = new HashMap<String, ArcProxy>();
 
-    private DirectedSparseMultigraph<NodeProxy, ArcProxy> graph = new DirectedSparseMultigraph<NodeProxy, ArcProxy>() {
+    private final DirectedSparseMultigraph<NodeProxy, ArcProxy> graph = new DirectedSparseMultigraph<NodeProxy, ArcProxy>() {
         private static final long serialVersionUID = 1L;
 
         @Override
